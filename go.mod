c0473c497dc597e50b0f51889b3cb86574eec425
AlexanderYekat <chemsaha@yandex.ru>
2024-01-31 19:20:40 +0500

    добавил проверку марок переб пробитием чека коррекции, не тестировал

--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module clientrabbit
 
 go 1.21.4
 
-require (
-	github.com/kardianos/service v1.2.2 // indirect
-	github.com/rabbitmq/amqp091-go v1.9.0 // indirect
-	golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
-)
+require golang.org/x/sys v0.16.0
--- jsontokkt.go
+++ jsontokkt.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	fptr10 "clientrabbit/fptr"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -11,7 +12,122 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
+
+type TClientInfo struct {
+	EmailOrPhone string `json:"emailOrPhone"`
+}
+
+type TTaxNDS struct {
+	Type string `json:"type,omitempty"`
+}
+type TProductCodesAtol struct {
+	Undefined    string `json:"undefined,omitempty"` //32 символа только
+	Code_EAN_8   string `json:"ean8,omitempty"`
+	Code_EAN_13  string `json:"ean13,omitempty"`
+	Code_ITF_14  string `json:"itf14,omitempty"`
+	Code_GS_1    string `json:"gs10,omitempty"`
+	Tag1305      string `json:"gs1m,omitempty"`
+	Code_KMK     string `json:"short,omitempty"`
+	Code_MI      string `json:"furs,omitempty"`
+	Code_EGAIS_2 string `json:"egais20,omitempty"`
+	Code_EGAIS_3 string `json:"egais30,omitempty"`
+	Code_F_1     string `json:"f1,omitempty"`
+	Code_F_2     string `json:"f2,omitempty"`
+	Code_F_3     string `json:"f3,omitempty"`
+	Code_F_4     string `json:"f4,omitempty"`
+	Code_F_5     string `json:"f5,omitempty"`
+	Code_F_6     string `json:"f6,omitempty"`
+}
+
+type TPayment struct {
+	Type string  `json:"type"`
+	Sum  float64 `json:"sum"`
+}
+
+type TGenearaPosAndTag11921191 struct {
+	Type string `json:"type"`
+}
+
+type TPosition struct {
+	Type            string   `json:"type"`
+	Name            string   `json:"name"`
+	Price           float64  `json:"price"`
+	Quantity        float64  `json:"quantity"`
+	Amount          float64  `json:"amount"`
+	MeasurementUnit string   `json:"measurementUnit"`
+	PaymentMethod   string   `json:"paymentMethod"`
+	PaymentObject   string   `json:"paymentObject"`
+	Tax             *TTaxNDS `json:"tax,omitempty"`
+	//fot type tag1192 //AdditionalAttribute
+	Value        string             `json:"value,omitempty"`
+	Print        bool               `json:"print,omitempty"`
+	ProductCodes *TProductCodesAtol `json:"productCodes,omitempty"`
+	ImcParams    *TImcParams        `json:"imcParams,omitempty"`
+}
+
+type TAnsweChekcMark struct {
+	Ready bool `json:"ready"`
+}
+
+type TBeginTaskMarkCheck struct {
+	Type   string     `json:"type"`
+	Params TImcParams `json:"params"`
+}
+
+type TItemInfoCheckResult struct {
+	ImcCheckFlag              bool `json:"imcCheckFlag"`
+	ImcCheckResult            bool `json:"imcCheckResult"`
+	ImcStatusInfo             bool `json:"imcStatusInfo"`
+	ImcEstimatedStatusCorrect bool `json:"imcEstimatedStatusCorrect"`
+	EcrStandAloneFlag         bool `json:"ecrStandAloneFlag"`
+}
+
+type TImcParams struct {
+	ImcType             string                `json:"imcType"`
+	Imc                 string                `json:"imc"`
+	ItemEstimatedStatus string                `json:"itemEstimatedStatus"`
+	ImcModeProcessing   int                   `json:"imcModeProcessing"`
+	ImcBarcode          string                `json:"imcBarcode,omitempty"`
+	ItemInfoCheckResult *TItemInfoCheckResult `json:"itemInfoCheckResult,omitempty"`
+	ItemQuantity        float64               `json:"itemQuantity,omitempty"`
+	ItemUnits           string                `json:"itemUnits,omitempty"`
+	NotSendToServer     bool                  `json:"notSendToServer,omitempty"`
+}
+
+type TTag1192_91 struct {
+	Type  string `json:"type"`
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
+	Print bool   `json:"print,omitempty"`
+}
+
+type TOperator struct {
+	Name  string `json:"name"`
+	Vatin string `json:"vatin,omitempty"`
+}
+
+// При работе по ФФД ≥ 1.1 чеки коррекции имеют вид, аналогичный обычным чекам, но с
+// добавлением информации о коррекции: тип, описание, дата документа основания и
+// номер документа основания.
+type TCorrectionCheck struct {
+	Type string `json:"type"` //sellCorrection - чек коррекции прихода
+	//buyCorrection - чек коррекции расхода
+	//sellReturnCorrection - чек коррекции возврата прихода (ФФД ≥ 1.1)
+	//buyReturnCorrection - чек коррекции возврата расхода
+	Electronically       bool        `json:"electronically"`
+	TaxationType         string      `json:"taxationType,omitempty"`
+	ClientInfo           TClientInfo `json:"clientInfo"`
+	CorrectionType       string      `json:"correctionType"` //
+	CorrectionBaseDate   string      `json:"correctionBaseDate"`
+	CorrectionBaseNumber string      `json:"correctionBaseNumber"`
+	Operator             TOperator   `json:"operator"`
+	//Items                []TPosition `json:"items"`
+	Items    []interface{} `json:"items"` //либо TTag1192_91, либо TPosition
+	Payments []TPayment    `json:"payments"`
+	Total    float64       `json:"total,omitempty"`
+}
 
 var DIROFJSONS = ".\\jsons\\works\\"
 
@@ -30,7 +146,7 @@ const LOGERROR = "error"
 const LOGSKIP_LINES = "skip_line"
 const LOGOTHER = "other"
 const LOG_PREFIX = "TASKS"
-const Version_of_program = "2024_01_30_02"
+const Version_of_program = "2024_01_31_01"
 
 const FILE_NAME_PRINTED_CHECKS = "printed.txt"
 const FILE_NAME_CONNECTION = "connection.txt"
@@ -38,6 +154,8 @@ const FILE_NAME_CONNECTION = "connection.txt"
 func main() {
 	var err error
 	var descrError string
+	var imcResultCheckin TItemInfoCheckResult
+	var answerOfCheckMark TAnsweChekcMark
 
 	runDescription := "программа версии " + Version_of_program + " распечатка чеков из json заданий запущена"
 	fmt.Println(runDescription)
@@ -168,6 +286,7 @@ func main() {
 	logsmap[LOGINFO_WITHSTD].Println("начинаем выполнять json чеков", countOfFiles)
 	logsmap[LOGINFO_WITHSTD].Println("всего json заданий для печати чека", countOfFiles)
 	for k, currFullFileName := range listOfFiles {
+		var receipt TCorrectionCheck
 		currNumIsprChecka := getFDFromFileName(currFullFileName)
 		logsmap[LOGINFO_WITHSTD].Printf("обработка задания %v из %v %v", k+1, countOfFiles, currFullFileName)
 		logsmap[LOGINFO].Printf("начинаем читать json файл %v", currFullFileName)
@@ -178,6 +297,86 @@ func main() {
 			continue
 		}
 		logsmap[LOGINFO].Printf("прочитали json файл %v", currFullFileName)
+		//проверяем есть в чеке марки
+		err = json.Unmarshal([]byte(jsonCorrection), &receipt)
+		if err != nil {
+			errorDescr := fmt.Sprintf("ошибка (%v) парсинга json задания чека %v атол", err, currFullFileName)
+			logsmap[LOGERROR].Println(errorDescr)
+			continue
+		}
+		for _, v := range receipt.Items {
+			typeItem := v.(TGenearaPosAndTag11921191).Type
+			if typeItem != "position" {
+				continue
+			}
+			currPos := v.(TPosition)
+			if currPos.ImcParams == nil {
+				continue
+			}
+			currMarkBase64 := currPos.ImcParams.Imc
+			resJson, err := sendCheckOfMark(fptr, currMarkBase64, currPos.Quantity, currPos.MeasurementUnit)
+			if err != nil {
+				errorDescr := fmt.Sprintf("ошибка (%v) запуска проверки марки %v для чека %v атол", err, currMarkBase64, currFullFileName)
+				logsmap[LOGERROR].Println(errorDescr)
+				continue
+			}
+			if !successCommand(resJson) {
+				errorDescr := fmt.Sprintf("ошибка (%v) запуска проверки марки %v для чека %v атол", resJson, currMarkBase64, currFullFileName)
+				logsmap[LOGERROR].Println(errorDescr)
+				continue
+			}
+			for i := 0; i < 10; i++ {
+				resJson, err := getStatusOfChecking(fptr)
+				if err != nil {
+					errorDescr := fmt.Sprintf("ошибка (%v) получения статуса проверки марки %v для чека %v атол", err, currMarkBase64, currFullFileName)
+					logsmap[LOGERROR].Println(errorDescr)
+					continue
+				}
+				if !successCommand(resJson) {
+					errorDescr := fmt.Sprintf("ошибка (%v) получения статуса проверки марки %v для чека %v атол", resJson, currMarkBase64, currFullFileName)
+					logsmap[LOGERROR].Println(errorDescr)
+					continue
+				}
+				err = json.Unmarshal([]byte(resJson), &answerOfCheckMark)
+				if err != nil {
+					errorDescr := fmt.Sprintf("ошибка (%v) парсинга ответа проверки марки %v для чека %v атол", err, currMarkBase64, currFullFileName)
+					logsmap[LOGERROR].Println(errorDescr)
+					continue
+				}
+				if answerOfCheckMark.Ready {
+					break
+				}
+				//пауза в 1 секунду
+				duration := time.Second
+				time.Sleep(duration)
+			}
+			//принимаем марку
+			resOfChecking, err := acceptMark(fptr)
+			if err != nil {
+				errorDescr := fmt.Sprintf("ошибка (%v) принятия марки %v для чека %v атол", err, currMarkBase64, currFullFileName)
+				logsmap[LOGERROR].Println(errorDescr)
+				continue
+			}
+			if !successCommand(resOfChecking) {
+				errorDescr := fmt.Sprintf("ошибка (%v) принятия марки %v для чека %v атол", resOfChecking, currMarkBase64, currFullFileName)
+				logsmap[LOGERROR].Println(errorDescr)
+				continue
+			}
+			err = json.Unmarshal([]byte(resOfChecking), &imcResultCheckin)
+			if err != nil {
+				errorDescr := fmt.Sprintf("ошибка (%v) парсинга ответа проверки марки %v для чека %v атол", err, currMarkBase64, currFullFileName)
+				logsmap[LOGERROR].Println(errorDescr)
+				continue
+			}
+			//заполняем данные о марке
+			currPos.ImcParams.ItemInfoCheckResult.ImcCheckFlag = imcResultCheckin.ImcCheckFlag
+			currPos.ImcParams.ItemInfoCheckResult.ImcCheckResult = imcResultCheckin.ImcCheckResult
+			currPos.ImcParams.ItemInfoCheckResult.ImcStatusInfo = imcResultCheckin.ImcStatusInfo
+			currPos.ImcParams.ItemInfoCheckResult.ImcEstimatedStatusCorrect = imcResultCheckin.ImcEstimatedStatusCorrect
+			//fmt.Println(k, v.(TGenearaPosAndTag11921191).Type)
+		}
+		//елси есть в чеке марки - то проверяем их
+		//меняем данные результата проверки сголасно полученным данным проверки
 		logsmap[LOGINFO].Printf("послыем команду печати чека кассу json файл %v", jsonCorrection)
 		resulOfCommand, err := sendComandeAndGetAnswerFromKKT(fptr, jsonCorrection)
 		if err != nil {
@@ -215,7 +414,7 @@ func sendComandeAndGetAnswerFromKKT(fptr *fptr10.IFptr, comJson string) (string,
 		if !*debugpr {
 			desrError := fmt.Sprintf("ошибка (%v) печати чека коррекции на кассовом аппарате", err)
 			logsmap[LOGERROR].Println(desrError)
-			logsmap[LOGINFO].Print("начало процедуры sendComandeAndGetAnswerFromKKT c ошибкой", err)
+			logsmap[LOGINFO].Print("конец процедуры sendComandeAndGetAnswerFromKKT c ошибкой", err)
 			return desrError, err
 		}
 	}
@@ -225,7 +424,113 @@ func sendComandeAndGetAnswerFromKKT(fptr *fptr10.IFptr, comJson string) (string,
 	//if *debugpr {
 	//}
 	//result := "{\"result\": \"all ok\"}"
-	logsmap[LOGINFO].Print("начало процедуры sendComandeAndGetAnswerFromKKT без ошибки")
+	logsmap[LOGINFO].Print("конец процедуры sendComandeAndGetAnswerFromKKT без ошибки")
+	return result, nil
+}
+
+func sendCheckOfMark(fptr *fptr10.IFptr, mark string, qnt float64, itemUnits string) (string, error) {
+	var err error
+	logsmap[LOGINFO].Print("начало процедуры sendCheckOfMark")
+	//return "", nil
+	comJson, err := getJsonOfBeginCheck(mark, qnt, itemUnits)
+	if err != nil {
+		desrError := fmt.Sprintf("ошибка (%v) фоормирования json задания проверки марки", err)
+		logsmap[LOGERROR].Println(desrError)
+		logsmap[LOGINFO].Print("конец процедуры sendCheckOfMark c ошибкой", err)
+		return "", err
+	}
+	fptr.SetParam(fptr10.LIBFPTR_PARAM_JSON_DATA, comJson)
+	if !*emulation {
+		err = fptr.ProcessJson()
+	}
+	if err != nil {
+		if !*debugpr {
+			desrError := fmt.Sprintf("ошибка (%v) выполнение команды начать проверку марки на кассовом аппарате", err)
+			logsmap[LOGERROR].Println(desrError)
+			logsmap[LOGINFO].Print("конец процедуры sendCheckOfMark c ошибкой", err)
+			return "", err
+		}
+	}
+	result := fptr.GetParamString(fptr10.LIBFPTR_PARAM_JSON_DATA)
+	logsmap[LOGOTHER].Println("comJson", comJson)
+	logsmap[LOGOTHER].Println("result", result)
+	//if *debugpr {
+	//}
+	//result := "{\"result\": \"all ok\"}"
+	logsmap[LOGINFO].Print("конец процедуры sendCheckOfMark без ошибки")
+	return "", nil
+}
+
+func getJsonOfBeginCheck(mark string, qnt float64, itemUnits string) (string, error) {
+	var begMarkStructur TBeginTaskMarkCheck
+	begMarkStructur.Type = "beginMarkingCodeValidation"
+	begMarkStructur.Params.Imc = mark
+	begMarkStructur.Params.ItemQuantity = qnt
+	begMarkStructur.Params.ItemUnits = itemUnits
+	begMarkStructur.Params.ImcType = "auto"
+	begMarkStructur.Params.ItemEstimatedStatus = "itemStatusUnchanged"
+	begMarkStructur.Params.ImcModeProcessing = 0
+	begMarkStructur.Params.NotSendToServer = false
+	resstr, err := json.MarshalIndent(begMarkStructur, "", "\t")
+	if err != nil {
+		desrError := fmt.Sprintf("ошибка (%v) формирования json задания проверки марки", err)
+		logsmap[LOGERROR].Println(desrError)
+		return "", err
+	}
+	return string(resstr), err
+}
+
+func getStatusOfChecking(fptr *fptr10.IFptr) (string, error) {
+	var err error
+	logsmap[LOGINFO].Print("начало процедуры getStatusOfChecking")
+	//return "", nil
+	comJson := "{\"type\": \"getMarkingCodeValidationStatus\"}"
+	fptr.SetParam(fptr10.LIBFPTR_PARAM_JSON_DATA, comJson)
+	if !*emulation {
+		err = fptr.ProcessJson()
+	}
+	if err != nil {
+		if !*debugpr {
+			desrError := fmt.Sprintf("ошибка (%v) выполнение команды принятия марки на кассовом аппарате", err)
+			logsmap[LOGERROR].Println(desrError)
+			logsmap[LOGINFO].Print("конец процедуры getStatusOfChecking c ошибкой", err)
+			return "", err
+		}
+	}
+	result := fptr.GetParamString(fptr10.LIBFPTR_PARAM_JSON_DATA)
+	logsmap[LOGOTHER].Println("comJson", comJson)
+	logsmap[LOGOTHER].Println("result", result)
+	//if *debugpr {
+	//}
+	//result := "{\"result\": \"all ok\"}"
+	logsmap[LOGINFO].Print("конец процедуры getStatusOfChecking без ошибки")
+	return result, nil
+}
+
+func acceptMark(fptr *fptr10.IFptr) (string, error) {
+	var err error
+	logsmap[LOGINFO].Print("начало процедуры acceptMark")
+	//return "", nil
+	comJson := "{\"type\": \"acceptMarkingCode\"}"
+	fptr.SetParam(fptr10.LIBFPTR_PARAM_JSON_DATA, comJson)
+	if !*emulation {
+		err = fptr.ProcessJson()
+	}
+	if err != nil {
+		if !*debugpr {
+			desrError := fmt.Sprintf("ошибка (%v) выполнение команды принятия марки на кассовом аппарате", err)
+			logsmap[LOGERROR].Println(desrError)
+			logsmap[LOGINFO].Print("конец процедуры acceptMark c ошибкой", err)
+			return "", err
+		}
+	}
+	result := fptr.GetParamString(fptr10.LIBFPTR_PARAM_JSON_DATA)
+	logsmap[LOGOTHER].Println("comJson", comJson)
+	logsmap[LOGOTHER].Println("result", result)
+	//if *debugpr {
+	//}
+	//result := "{\"result\": \"all ok\"}"
+	logsmap[LOGINFO].Print("конец процедуры acceptMark без ошибки")
 	return result, nil
 }
 
@@ -346,17 +651,17 @@ func readJsonFromFile(currFullFileName string) (string, error) {
 
 func successCommand(resulJson string) bool {
 	res := true
-	indOsh := strings.Index(resulJson, "ошибка")
-	indErr := strings.Index(resulJson, "error")
-	if indErr != -1 || indOsh != -1 {
+	indOsh := strings.Contains(resulJson, "ошибка")
+	indErr := strings.Contains(resulJson, "error")
+	if indErr || indOsh {
 		res = false
 	}
 	return res
-}
+} //successCommand
 
 func connectWithKassa(fptr *fptr10.IFptr, comport string) bool {
 	sComPorta := comport
-	if strings.Index(comport, "COM") == -1 {
+	if !strings.Contains(comport, "COM") {
 		sComPorta = "COM" + comport
 	}
 	fptr.SetSingleSetting(fptr10.LIBFPTR_SETTING_MODEL, strconv.Itoa(fptr10.LIBFPTR_MODEL_ATOL_AUTO))
